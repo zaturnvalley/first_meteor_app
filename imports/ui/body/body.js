80e442c45d771754075c0ddbc928338ffda75b01
zaturnvalley <zacharycmason@gmail.com>
2016-09-26 15:26:04 -0700

    added basic update delete functionality, need to test

--- imports/ui/body/body.js
+++ imports/ui/body/body.js
@@ -23,5 +23,25 @@ Template.body.events({
     });
 
     target.text.value = '';
+  },
+  'submit .update'(event){
+    event.preventDefault();
+
+    const targ = event.target;
+    const updateUrl = targ.text.value;
+
+    Images.update({
+      url
+    })
+  },
+  'delete .delete'(event){
+    event.preventDefault();
+
+    const tar = event.target;
+    const deleteUrl = tar.text.value;
+
+    Images.remove({
+      url
+    })
   }
 });
