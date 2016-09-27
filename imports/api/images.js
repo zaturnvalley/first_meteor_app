755b362ec47db11a2cfb230487b59b2bdd516cd4
zaturnvalley <zacharycmason@gmail.com>
2016-09-27 13:18:31 -0700

    added method, ommited insecure

--- imports/api/images.js
+++ imports/api/images.js
@@ -1,3 +1,19 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 export const Images = new Mongo.Collection('images');
+
+Meteor.methods({
+  'images.insert'(url) {
+    if(!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Images.insert({
+      url,
+      createdAt: new Date(),
+      owner: this.userId,
+      username: Meteor.users.findOne(this.userId).username
+    })
+  }
+});
